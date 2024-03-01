9517ba02cea9c5d3c47ad0d5e001aa03a07f6e57
Frata Mamud <e2395117@cmaisonneuve.qc.ca>
2024-03-01 15:02:44 -0500

    Travail sur le changement de couleur

--- src/components/Entete/Entete.js
+++ src/components/Entete/Entete.js
@@ -12,39 +12,42 @@ function Entete(props) {
   return (
     // em sala de aula
 
-    <header className="pb-small pt-small">
-      <NavLink to="/"><h1>Video Club</h1></NavLink>    
-      <nav>
-        {/* {props.estLog ? <NavLink to="/admin">Admin</NavLink> : ''} */}
-        {/* {props.logging.estLog ? <NavLink to="/admin">Admin</NavLink> : ''} */}
-        {context.estLog ? <NavLink to="/admin">Admin</NavLink> : ''}
-        <NavLink to="/liste-films">Liste des Films</NavLink>
-      </nav>
+    // <header className="pb-small pt-small">
+    //   <NavLink to="/"><h1>Video Club</h1></NavLink>    
+    //   <nav>
+    //     {/* {props.estLog ? <NavLink to="/admin">Admin</NavLink> : ''} */}
+    //     {/* {props.logging.estLog ? <NavLink to="/admin">Admin</NavLink> : ''} */}
+    //     {/* {context.estLog ? <NavLink to="/admin">Admin</NavLink> : ''} */}
+    //     <NavLink to="/liste-films">Liste des Films</NavLink>
+    //   </nav>
 
-      <form onSubmit={props.handleLogin}>
-        <input type='text' name="usager" placeholder="Login"></input>
-        <button>Login</button>
-      </form>
-    </header>
-
-    // <header>
-    //     <div className="logo-container">
-    //         <NavLink to="/">
-    //           <img className="logo" src="../../../icones/logo.svg" alt="" />
-    //           </NavLink>
-    //     </div>
-    //     <nav>
-    //         <ul>
-    //             <li>
-    //                 {/* <a class="active" href="accueil.html">Liste des Films</a> */}
-    //                 <NavLink className="active" to="/liste-films">Liste des Films</NavLink>
-    //             </li>
-    //             <li>
-    //               <a className="active" href="accueil.html">Login</a>
-    //             </li>
-    //         </ul>
-    //     </nav>
+    //   <form onSubmit={props.handleLogin}>
+    //     <input type='text' name="usager" placeholder="Login"></input>
+    //     <button>Login</button>
+    //   </form>
     // </header>
+
+    <header>
+        <div className="logo-container">
+            <NavLink to="/">
+              {/* <img className="logo" src="../../../icones/logo.svg" alt="" /> */}
+              </NavLink>
+        </div>
+        <nav>
+            <ul>
+                <li>
+                    {/* <a class="active" href="accueil.html">Liste des Films</a> */}
+                    <NavLink className="active" to="/liste-films">Liste des Films</NavLink>
+                </li>
+                <li>
+                <form onSubmit={props.handleLogin}>
+                  <input type='text' name="usager" placeholder="Login"></input>
+                  <button>Login</button>
+                </form>
+                </li>
+            </ul>
+        </nav>
+    </header>
   );
 }
 
